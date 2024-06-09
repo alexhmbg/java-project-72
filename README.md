5470faa38d40d122ada15ae6feac22ab112e3720
Alexande Hamburg <uplink205@gmail.com>
2024-06-09 18:52:49 +0300

    update

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/alexhmbg/java-project-72/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/alexhmbg/java-project-72/actions)
+https://java-project-72-st6n.onrender.com/
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    id("application")
     id("java")
+    id("application")
+    id("com.github.johnrengelman.shadow") version "8.1.1";
 }
 
 group = "hexlet.code"
@@ -17,8 +18,8 @@ application {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("org.slf4j:slf4j-simple:2.0.13")
 
+    implementation("org.slf4j:slf4j-simple:2.0.13")
     implementation("io.javalin:javalin:6.1.6")
 }
 
