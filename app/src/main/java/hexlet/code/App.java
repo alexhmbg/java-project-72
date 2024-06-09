600d1f0d911e94a1c5eefbb887508945703c4608
Alexande Hamburg <uplink205@gmail.com>
2024-06-09 19:55:00 +0300

    add schema.sql, update connection

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,13 +5,20 @@ import com.zaxxer.hikari.HikariDataSource;
 import hexlet.code.repository.BaseRepository;
 import io.javalin.Javalin;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.sql.SQLException;
+import java.util.stream.Collectors;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, IOException {
         Javalin app = getApp();
         app.start();
     }
 
-    public static Javalin getApp() {
+    public static Javalin getApp() throws SQLException, IOException {
         var hikariConfig = new HikariConfig();
         String jdbcDatabaseUrl = System.getenv()
                 .getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:project");
@@ -19,6 +26,13 @@ public class App {
         hikariConfig.setJdbcUrl(jdbcDatabaseUrl);
 
         var dataSource = new HikariDataSource(hikariConfig);
+
+        var sql = readResourceFile("schema.sql");
+        try (var connection = dataSource.getConnection();
+             var statement = connection.createStatement()) {
+            statement.execute(sql);
+        }
+
         BaseRepository.dataSource = dataSource;
 
         var app = Javalin.create(config -> {
@@ -27,5 +41,12 @@ public class App {
 
         app.get("/", ctx -> ctx.result("Hello World"));
         return app;
+    }
+
+    private static String readResourceFile(String fileName) throws IOException {
+        var inputStream = App.class.getClassLoader().getResourceAsStream(fileName);
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            return reader.lines().collect(Collectors.joining("\n"));
+        }
     }
 }
