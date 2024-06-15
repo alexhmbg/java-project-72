aa1525be98de6abaf8f8dd2a687ac4ea4132e52b
Alexande Hamburg <uplink205@gmail.com>
2024-06-15 16:53:51 +0300

    update

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -23,47 +23,50 @@ import gg.jte.ContentType;
 import gg.jte.TemplateEngine;
 import io.javalin.rendering.template.JavalinJte;
 
-
 @Slf4j
 public class App {
-    private static int getPort() {
+
+    private static TemplateEngine createTemplateEngine() {
+        ClassLoader classLoader = App.class.getClassLoader();
+        ResourceCodeResolver codeResolver = new ResourceCodeResolver("templates", classLoader);
+        TemplateEngine templateEngine = TemplateEngine.create(codeResolver, ContentType.Html);
+        return templateEngine;
+    }
+
+    static int getPort() {
         String port = System.getenv().getOrDefault("PORT", "7070");
         return Integer.valueOf(port);
     }
 
-    private static String getDatabaseUrl() {
-        return System.getenv().getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:project");
+    static String getBD() {
+        String bd = System.getenv().getOrDefault("JDBC_DATABASE_URL",
+                "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
+        return bd;
     }
 
-    private static String getMode() {
-        return System.getenv().getOrDefault("APP_ENV", "development");
-    }
 
-    private static boolean isProduction() {
-
-        return getMode().equals("production");
-    }
-
-    private static InputStream getFileFromResourceAsStream(String fileName) {
-        ClassLoader classLoader = App.class.getClassLoader();
-        InputStream is = classLoader.getResourceAsStream(fileName);
-        return is;
-    }
-
-    private static String getContentFromStream(InputStream is) throws IOException {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8))) {
-            return reader.lines().collect(Collectors.joining("\n"));
+    private static String getContent(InputStream is) throws IOException {
+        try (var reader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8))) {
+            return reader.lines().collect(Collectors.joining(System.lineSeparator()));
         }
     }
 
+    private static InputStream getFile(String fileName) {
+        var classLoader = App.class.getClassLoader();
+        var inputStream = classLoader.getResourceAsStream(fileName);
+        return inputStream;
+    }
+
+
     public static Javalin getApp() throws IOException, SQLException {
+
         var hikariConfig = new HikariConfig();
-        hikariConfig.setJdbcUrl(getDatabaseUrl());
-        hikariConfig.setUsername(System.getenv("JDBC_DATABASE_USERNAME"));
-        hikariConfig.setPassword(System.getenv("JDBC_DATABASE_PASSWORD"));
+        hikariConfig.setJdbcUrl(getBD());
 
         var dataSource = new HikariDataSource(hikariConfig);
-        String sql = getContentFromStream(getFileFromResourceAsStream("schema.sql"));
+        var sql = getContent(getFile("schema.sql"));
+
+        log.info(sql);
 
         try (var connection = dataSource.getConnection();
              var statement = connection.createStatement()) {
@@ -77,10 +80,6 @@ public class App {
             config.fileRenderer(new JavalinJte(createTemplateEngine()));
         });
 
-        app.before(ctx -> {
-            ctx.contentType("text/html; charset=utf-8");
-        });
-
         app.get(NamedRoutes.rootPath(), RootController::index);
         app.get(NamedRoutes.urlsPath(), UrlsController::index);
         app.get(NamedRoutes.urlPath("{id}"), UrlsController::show);
@@ -90,14 +89,8 @@ public class App {
         return app;
     }
 
-    private static TemplateEngine createTemplateEngine() {
-        ClassLoader classLoader = App.class.getClassLoader();
-        ResourceCodeResolver codeResolver = new ResourceCodeResolver("templates", classLoader);
-        return TemplateEngine.create(codeResolver, ContentType.Html);
-    }
-
-    public static void main(String[] args) throws SQLException, IOException {
-        Javalin app = getApp();
+    public static void main(String[] args) throws IOException, SQLException {
+        var app = getApp();
         app.start(getPort());
     }
 }
